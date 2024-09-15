d8c7f4da374041b9d0d69d444e1e149695f2db9b
pedro <pedrosbmartinazzo@hotmail.com>
2024-09-14 23:34:20 -0300

    nova migration

---
+++ src/main/resources/db/migration/V4__updates_produto.sql
@@ -0,0 +1,8 @@
+update produto set imagem = "https://imgs.casasbahia.com.br/55065634/1g.jpg" where nome = "Smartphone";
+update produto set imagem = "https://i.zst.com.br/thumbs/12/3d/34/-1162794238.jpg" where nome = "Notebook";
+update produto set imagem = "https://http2.mlstatic.com/D_NQ_NP_903754-MLB76993320891_062024-O.webp" where nome = "Camiseta";
+update produto set imagem = "https://img.lojadomecanico.com.br/IMAGENS/49/840/168416/1619098321402.JPG" where nome = "Calça Jeans";
+update produto set imagem = "https://static.leleomoveis.com.br/public/leleomoveis/imagens/produtos/fone-de-ouvido-multilaser-ph246-bluetooth-preto-5716.jpg" where nome = "Fone de Ouvido";
+update produto set imagem = "https://a-static.mlcdn.com.br/450x450/televisao-24-polegadas-multilaser-preto-hd-hdmi-usb-tl037/loibrasil/12267/e5f49ec20c9034620440b255ced0bd5a.jpeg" where nome = "Televisão";
+update produto set imagem = "https://images.tcdn.com.br/img/img_prod/826403/tenis_mizuno_wave_invictus_4_preto_ouro_masculino_9699_1_27b7588f0101466ee89c097d694d3316.jpg" where nome = "Tênis";
+update produto set imagem = "https://trilhaesportes.fbitsstatic.net/img/p/jaqueta-adidas-essentials-woven-3-stripes-masculina-preto-branco-71713/271983-1.jpg?w=800&h=800&v=no-value" where nome = "Jaqueta";
